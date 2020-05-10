6e4289e380626d653d3b47a3adcc903e59133bac
Douglas Matoso <doug2k1@gmail.com>
2020-05-10 16:21:01 -0300

    Add login form

---
+++ src/LoginForm.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import "./LoginForm.css";
+
+const LoginForm = () => {
+  const { register, handleSubmit, errors } = useForm();
+
+  function onSubmit(data) {
+    console.log("Data submitted: ", data);
+  }
+
+  return (
+    <div className="login-form">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="inputEmail">E-mail</label>
+        <input
+          type="email"
+          id="inputEmail"
+          name="email"
+          ref={register({
+            required: "Enter your e-mail",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+              message: "Enter a valid e-mail address",
+            },
+          })}
+        />
+        {errors.email && <p className="error">{errors.email.message}</p>}
+
+        <label htmlFor="inputPassword">Password</label>
+        <input
+          type="password"
+          id="inputPassword"
+          name="password"
+          ref={register({ required: "Enter your password" })}
+        />
+        {errors.password && <p className="error">{errors.password.message}</p>}
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
